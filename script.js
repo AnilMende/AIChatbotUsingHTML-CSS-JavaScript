b545153f713a4bb5ca522fecc18fc893da9d7bfc
Anil <anilmende2002@email.com>
2025-08-16 18:54:36 +0530

    v6

---
+++ script.js
@@ -0,0 +1,44 @@
+const messageInput = document.querySelector(".message-input")
+const chatBody = document.querySelector(".chat-body");
+const sendMessageButton = document.querySelector("#send-message");
+
+
+const userData = {
+    message:null
+}
+
+
+
+// create a message element with dynamic classes and return it
+const createMessageElement = (content,classes) => {
+    const div = document.createElement("div");
+
+    div.classList.add("message", classes);
+    div.innerHTML = content;
+    return div;
+}
+
+const handleOutgoingMessage = (e) => {
+    e.preventDefault();
+    userData.message = messageInput.value.trim()
+
+    // create and display user image
+    const messageContent = `<div class="message-text">${userData.message}</div>`;
+    const outgoingMessageDiv = createMessageElement(messageContent,"user-message");
+
+    chatBody.appendChild(outgoingMessageDiv);
+}
+
+// Handling the Enter Key press for sending messages
+
+messageInput.addEventListener("keydown", (e) => {
+    const userMessage = e.target.value.trim();
+
+    if(e.key === "Enter" && userMessage){
+        // handleOutgoingMessage(userMessage);
+        handleOutgoingMessage(e)
+    }
+});
+
+
+sendMessageButton.addEventListener("click",(e) => handleOutgoingMessage(e))
